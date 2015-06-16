4a39597a66199d09b1ffd91df52e5aebb99edeba
Timo Flöttmann <timo.floettmann@gmail.com>
2015-06-16 16:06:11 +0200

    import version 3.0.4

---
+++ README.md
@@ -0,0 +1,7 @@
+Wrapper Extension for the Amazon Web Services PHP SDK Version 3.
+
+The Original SDK Repository can be found here: https://github.com/aws/aws-sdk-php
+
+And the SDK Documentation here: http://docs.aws.amazon.com/aws-sdk-php/v3/api/index.html
+
+TYPO3 Extension Repository Link: http://typo3.org/extensions/repository/view/aws_sdk
---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Amazon Web Services SDK',
+	'description' => 'Wrapper Extension for the Amazon Web Services PHP SDK',
+	'category' => 'module',
+	'author' => 'Timo Flöttmann',
+	'author_email' => 'timo.floettmann@gmail.com',
+	'author_company' => 'ONDIGO GmbH',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '3.0.4',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.5.0-5.6.99',
+			'typo3' => '6.2.0-7.99.99'
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
